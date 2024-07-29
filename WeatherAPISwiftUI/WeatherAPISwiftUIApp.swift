79932db87b40b163a1efab3009c4e2dc202f12e6
Brian Surface <brian.surface@outlook.com>
2024-07-29 07:14:59 -0400

    Initial Commit

---
+++ WeatherAPISwiftUI/WeatherAPISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPISwiftUIApp.swift
+//  WeatherAPISwiftUI
+//
+//  Created by Brian Surface on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
