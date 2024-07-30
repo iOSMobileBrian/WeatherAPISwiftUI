34d8927bd9fc8da7844a08dc8afc85ff916a9db6
Brian Surface <brian.surface@outlook.com>
2024-07-30 08:33:36 -0400

    Update ContentView.swift

--- WeatherAPISwiftUI/ContentView.swift
+++ WeatherAPISwiftUI/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var weatherVM: WeatherViewModel
+    
+    init() {
+        self.weatherVM = WeatherViewModel()
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack( alignment: .center){
+            
+            TextField("Enter city name", text: self.$weatherVM.cityName){
+                self.weatherVM.search()
+            }.font(.largeTitle)
+                .padding()
+                .fixedSize()
+            
+            Text(self.weatherVM.temperature)
+                .font(.custom("Arial", size: 100))
+                .foregroundColor(.white)
+                .offset(y: 100)
+                .padding()
+        }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .background(Color.green)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
